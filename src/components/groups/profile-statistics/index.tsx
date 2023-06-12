43780447c7a857fe6265dbf0f71609fd639bbf70
tak <tmhonde45@gmail.com>
2023-06-12 15:43:08 -0700

    fix: mutations and queries

--- src/components/groups/profile-statistics/index.tsx
+++ src/components/groups/profile-statistics/index.tsx
@@ -5,18 +5,7 @@ import Statistic from '../../data-display/statistic';
 function ProfileStatistics({ profile }: { profile: IProfile }) {
   return (
     <HStack gap={3}>
-      {profile.accountType === 'ARTIST' && (
-        <Statistic value={0} label='followers' />
-      )}
-      {profile.accountType === 'FAN' && (
-        <Statistic value={0} label='friends' />
-      )}
-      {profile.holdrs && (
-        <Statistic value={profile.holdrs} label='holdrs' />
-      )}
-      {profile.memberships && (
-        <Statistic value={profile.memberships} label='memberships' />
-      )}
+      <Statistic value={profile.followers} label='followers' />
     </HStack>
   );
 }
