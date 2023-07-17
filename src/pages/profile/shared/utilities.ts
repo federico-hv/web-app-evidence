f6573ab517f66246fa9e4967662f9803cf47bb4f
tak <tmhonde45@gmail.com>
2023-07-17 11:24:04 -0700

    fix: [following] add mutual following
    Signed-off-by: tak <tmhonde45@gmail.com>

---
+++ src/pages/profile/shared/utilities.ts
@@ -0,0 +1,20 @@
+import { IUser } from '../../../shared';
+
+/**
+ * Get text to display next to the mutual user avatars
+ *
+ * @param users mutual users
+ * @param total total number of mutual users
+ */
+export function getMutualFollowersText(users: IUser[], total = 0): string {
+  if (total > 2) {
+    return `Followed by ${users[0].displayName}, ${
+      users[1].displayName
+    } and ${total - 2} more.`;
+  } else if (total === 2) {
+    return `Followed by ${users[0].displayName} and ${users[1].displayName}.`;
+  } else if (total === 1) {
+    return `Followed by ${users[0].displayName}.`;
+  }
+  return '';
+}
