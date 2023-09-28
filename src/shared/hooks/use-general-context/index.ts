71a17fbe6730574564016db23d46d1a64860e3de
tak <tmhonde45@gmail.com>
2023-09-27 17:31:03 -0700

    feat: [ui] mobile layout
    Signed-off-by: tak <tmhonde45@gmail.com>

--- src/shared/hooks/use-general-context/index.ts
+++ src/shared/hooks/use-general-context/index.ts
@@ -3,7 +3,7 @@ import { GeneralContext } from '../../contexts';
 
 export function useGeneralContext<T>(): {
   state: T;
-  update: (next: Partial<T>) => void;
+  update: (next?: Partial<T>) => void;
 } {
   return useContext(GeneralContext);
 }
