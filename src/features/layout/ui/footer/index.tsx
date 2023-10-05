f46fe382e0d4e0a99736eb084b0e66b332cedd88
tak <tmhonde45@gmail.com>
2023-10-04 18:33:03 -0700

    feat: [navigation] update mobile options
    Signed-off-by: tak <tmhonde45@gmail.com>

--- src/features/layout/ui/footer/index.tsx
+++ src/features/layout/ui/footer/index.tsx
@@ -1,5 +1,12 @@
-import { Box, HStack, NavigationLink } from '@holdr-ui/react';
 import {
+  Box,
+  HStack,
+  IconButton,
+  NavigationLink,
+  useDisclosure,
+} from '@holdr-ui/react';
+import {
+  DialogTabContextProvider,
   Paths,
   prefix,
   Responsive,
@@ -9,12 +16,31 @@ import {
   useScrollPosition,
 } from '../../../../shared';
 import { Link, matchPath, useLocation } from 'react-router-dom';
+import { css } from '../../../../configs';
+import { CreatePostDialog } from '../../../feeds';
+import { useState } from 'react';
+import { useCurrentUser } from '../../../auth';
+
+const largeIcon = css({
+  fontSize: '1.25rem !important',
+});
 
 function Footer() {
+  const currentUser = useCurrentUser();
+
   const { top } = useScrollPosition('#root');
   const isBottom = useIsBottomOf('#root');
   const { direction, delta } = useScrollDirection('#root');
+
   const { pathname } = useLocation();
+
+  const [option, setOption] = useState('');
+  const { isOpen, onOpen: open, onClose } = useDisclosure();
+
+  const onOpen = (option: string) => {
+    setOption(option);
+    open();
+  };
 
   return (
     <Responsive>
@@ -39,7 +65,7 @@ function Footer() {
             boxShadow: 'rgba(0, 0, 0, 0.15) 0px 4px 12px',
           }}
         >
-          <HStack justify='space-between' as='nav' p={2}>
+          <HStack justify='space-between' items='center' as='nav' p={2}>
             <NavigationLink
               variant='ghost'
               as={<Link to={prefix('/', Paths.root)} />}
@@ -54,23 +80,37 @@ function Footer() {
             />
             <NavigationLink
               variant='ghost'
-              as={<Link to={prefix('/', Paths.bookmarks)} />}
-              isActive={!!matchPath(Paths.bookmarks, pathname)}
-              activeIcon='bookmark-fill'
+              as={<Link to={prefix('/', Paths.messages)} />}
+              isActive={!!matchPath(Paths.messages, pathname)}
+              activeIcon='chat-alt-fill'
               size='sm'
-              inactiveIcon='bookmark-outline'
+              inactiveIcon='chat-alt-outline'
               css={{
                 padding: 0,
                 size: '2.5rem',
               }}
             />
+            {currentUser && currentUser.role === 'artist' && (
+              <DialogTabContextProvider
+                value={{ isOpen, onOpen, option, onClose }}
+              >
+                <IconButton
+                  size='lg'
+                  onClick={() => onOpen('')}
+                  className={largeIcon()}
+                  icon={'add'}
+                  ariaLabel={'create post'}
+                />
+                <CreatePostDialog />
+              </DialogTabContextProvider>
+            )}
             <NavigationLink
               variant='ghost'
-              as={<Link to={prefix('/', Paths.releases)} />}
-              isActive={!!matchPath(Paths.releases, pathname)}
-              activeIcon='releases-fill'
+              as={<Link to={prefix('/', Paths.notifications)} />}
+              isActive={!!matchPath(Paths.notifications, pathname)}
+              activeIcon='notification-alt-fill'
               size='sm'
-              inactiveIcon='releases-outline'
+              inactiveIcon='notification-alt-outline'
               css={{
                 padding: 0,
                 size: '2.5rem',
