afcc88abfe2cf017f267828f1441cfeea7bee777
Babak <hadadybabak@gmail.com>
2023-10-27 15:05:18 -0700

    refactor: update calendar style

--- src/shared/components/calendar/ui/calendar-date.tsx
+++ src/shared/components/calendar/ui/calendar-date.tsx
@@ -1,23 +1,57 @@
-import { Button, Container, Text } from '@holdr-ui/react';
+import { Box, Button, Container, Text } from '@holdr-ui/react';
 import { DateProps } from '../types';
 import _ from 'lodash';
+import { css } from 'configs';
+
+const baseStyle = css({
+  // padding: '16px 4px',
+  alignContent: 'center',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '35px',
+  width: '31px',
+  fontWeight: '$400',
+  margin: 'auto',
+  fontSize: '50px',
+});
 
 function Date({ date, initialDate, onClick, disabled }: DateProps) {
+  let props = {};
+
+  const disabledProps = {
+    css: {
+      color: 'gray',
+      cursor: 'default'
+    },
+  };
+
+  const selectedProps = {
+    css: {
+      backgroundColor: '$base800',
+      color: '$base100',
+    },
+    _hover: {
+      backgroundColor: '$base500',
+    },
+  };
+
+  const baseProps = {
+    _hover: {
+      backgroundColor: '$base200',
+    },
+  };
+
+  props = disabled
+    ? { ...props, ...disabledProps }
+    : { ...props, ...baseProps };
+
+  if (_.isEqual(date, initialDate)) props = { ...props, ...selectedProps };
+
   return (
-    <Container centerContent style={{ padding: 0 }}>
-      <Button
-        size='sm'
-        onClick={onClick}
-        radius={3}
-        style={{ padding: '16px 4px', width: '28px' }}
-        {...(!_.isEqual(initialDate, date) && {
-          disabled: disabled,
-          variant: 'ghost',
-        })}
-      >
-        <Text weight={400}>{date.day}</Text>
-      </Button>
-    </Container>
+    <Box {...props} onClick={onClick} radius={3} className={baseStyle()}>
+      {date.day}
+    </Box>
   );
 }
 
