a411162a57298aa5cd905e4433ae14b40e537939
Babak <hadadybabak@gmail.com>
2023-10-30 15:41:17 -0700

    refactor: format

--- src/shared/utilities/date.ts
+++ src/shared/utilities/date.ts
@@ -14,37 +14,6 @@ export class DateUtility {
     );
   }
 
-  static parseToIntMonth(month: string) {
-    switch (month.toLowerCase()) {
-      case 'january':
-        return 0;
-      case 'february':
-        return 1;
-      case 'march':
-        return 2;
-      case 'april':
-        return 3;
-      case 'may':
-        return 4;
-      case 'june':
-        return 5;
-      case 'july':
-        return 6;
-      case 'august':
-        return 7;
-      case 'september':
-        return 8;
-      case 'october':
-        return 9;
-      case 'november':
-        return 10;
-      case 'december':
-        return 11;
-      default:
-        return -1;
-    }
-  }
-
   static daysInMonth(month: string, year: string) {
     return dayjs(
       this.fromBreakdown({
