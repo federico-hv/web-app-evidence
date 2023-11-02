da286ca1264d8644bb116b602bb4c90d6e91c772
Babak <hadadybabak@gmail.com>
2023-11-02 13:53:25 -0700

    feat: [notification] finish prototype

--- src/features/notifications/ui/headers/section-header.tsx
+++ src/features/notifications/ui/headers/section-header.tsx
@@ -4,7 +4,7 @@ import { Paths } from 'shared';
 
 function SectionHeader() {
   return (
-    <HStack justify='space-between'>
+    <HStack justify='space-between' pb={4}>
       <Text weight={500}>New</Text>
       <Link to={Paths.notifications}>
         <Center>
