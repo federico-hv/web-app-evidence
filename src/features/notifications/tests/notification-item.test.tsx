1df2827655b96c4f0013ecd4de12a363938602e4
Babak <hadadybabak@gmail.com>
2023-11-06 10:05:03 -0800

    test: [notification] add tests

--- src/features/notifications/tests/notification-item.test.tsx
+++ src/features/notifications/tests/notification-item.test.tsx
@@ -1,26 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import NotificationItem, {
-  NotificationActionWrapper,
+  NotificationActionButton,
   NotificationAvatar,
   NotificationDetails,
   NotificationMediaItem,
-} from '../ui/notification-item/notification-item';
+} from '../ui/notification-item';
 
-import { Button } from '@holdr-ui/react';
+import logo from '../../../assets/images/logo.png';
 
 describe('NotificationItem', () => {
-  it('displays a notification avatar', () => {
-    render(<NotificationAvatar src='https://picsum.photos/200' />);
+  it('renders a NotificationAvatar component', () => {
+    render(<NotificationAvatar avatarImage='https://picsum.photos/200' />);
     const avatar = screen.getByAltText('Avatar');
     expect(avatar).to.exist;
   });
 
-  it('displays notification details', () => {
+  it('renders a NotificationDetails component', () => {
     render(
       <NotificationDetails
         name='John'
         description='Some description'
-        date={new Date()}
+        timeFromNow='2 hours ago'
       />,
     );
 
@@ -32,40 +32,54 @@ describe('NotificationItem', () => {
     expect(timeFromNow).to.exist;
   });
 
-  it('displays a notification image', () => {
+  it('renders a NotificationMediaItem component', () => {
     const { getByAltText } = render(
-      <NotificationMediaItem src='https://picsum.photos/200' />,
+      <NotificationMediaItem mediaItem='https://picsum.photos/200' />,
     );
     const mediaItem = getByAltText('notification media item');
     expect((mediaItem as HTMLMediaElement).src).to.equal('test-media.jpg');
   });
 
-  it('displays a notification action', () => {
+  it('renders a NotificationActionButton component', () => {
     let count = 0;
 
     const onClickMock = () => count++;
     render(
-      <NotificationActionWrapper>
-        <Button onClick={onClickMock}>Test Action Button</Button>
-      </NotificationActionWrapper>,
+      <NotificationActionButton onClick={onClickMock}>
+        Test Action Button
+      </NotificationActionButton>,
     );
     const actionButton = screen.getByText('Test Action Button');
     expect(actionButton).to.exist;
   });
 
-  it('renders a complete notification', () => {
+  it('has a functional onClick', () => {
+    let count = 0;
+
+    const onClickMock = () => count++;
+    render(
+      <NotificationActionButton onClick={onClickMock}>
+        Test Action Button
+      </NotificationActionButton>,
+    );
+    const actionButton = screen.getByText('Test Action Button');
+    fireEvent.click(actionButton);
+    expect(count).to.equal(1);
+  });
+
+  it('renders a NotificationItem component', () => {
     render(
       <NotificationItem>
-        <NotificationItem.Avatar src='test-avatar.jpg' />
+        <NotificationItem.Avatar avatarImage='test-avatar.jpg' />
         <NotificationItem.Details
           name='John Doe'
           description='Some notification description'
-          date={new Date()}
+          timeFromNow='2 hours ago'
         />
-        <NotificationItem.ActionWrapper>
-          <Button onClick={() => {}}>Action</Button>
-        </NotificationItem.ActionWrapper>
-        <NotificationItem.MediaItem src='test-media.jpg' />
+        <NotificationItem.ActionButton onClick={() => {}}>
+          Action
+        </NotificationItem.ActionButton>
+        <NotificationItem.MediaItem mediaItem='test-media.jpg' />
       </NotificationItem>,
     );
 
