2a9faaffdfdd519c47693611928c1446f549e83d
Babak <hadadybabak@gmail.com>
2023-11-09 11:23:06 -0800

    feat: [editor] clone editor

--- src/shared/components/editor/shared/plugins/mentions/mentions-plugin.tsx
+++ src/shared/components/editor/shared/plugins/mentions/mentions-plugin.tsx
@@ -14,6 +14,7 @@ import {
 import * as ReactDOM from 'react-dom';
 import { checkForAtSignMentions } from './regex';
 import { $createMentionNode } from '../../nodes/mention/mention-node';
+import { GenericOption, MentionOption, MentionsProps } from '../../types';
 
 export default function MentionsPlugin<T extends GenericOption>({
   dataFetcher,
