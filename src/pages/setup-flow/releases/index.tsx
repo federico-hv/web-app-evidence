65016dd4dbaa219eccf45b241a51aa0a29a865de
Takudzwa Mhonde <tmhonde45@gmail.com>
2023-11-16 15:05:36 -0800

    feat: disable continue button when user has not connected an account
    Signed-off-by: Takudzwa Mhonde <tmhonde45@gmail.com>

--- src/pages/setup-flow/releases/index.tsx
+++ src/pages/setup-flow/releases/index.tsx
@@ -8,6 +8,7 @@ import {
   CommonDialogContent,
   CommonDialogHeader,
   GeneralContextProvider,
+  GQLRenderer,
   Head,
   makePath,
   Paths,
@@ -20,31 +21,13 @@ import {
 import { IUnconnectedDialogContext, StepNumber } from './shared';
 import { getStepNumber } from './shared/utility';
 import { StepMap } from './shared/constants';
+import { useConnectedAccounts } from '../../../features';
 
 function SetupReleasesFlow() {
-  const disclosure = useDisclosure(true);
-
   const { openWith } = useToast();
-
-  const [height, setHeight] = useState('550px');
-  const [state, update] = useRecordState<IUnconnectedDialogContext>({
-    ids: [],
-  });
 
   const location = useLocation();
   const navigate = useNavigate();
-
-  const paths = location.pathname.split('/');
-  const currentPath = paths[paths.length - 1];
-  const stepNumber = getStepNumber(currentPath);
-
-  const onClose = () => {
-    const previousLocation = location.state?.previousLocation;
-
-    update({ ids: [] });
-    setHeight('550px');
-    navigate(previousLocation || prefix('/', Paths.releases));
-  };
 
   // Error handling
   useEffect(() => {
@@ -74,68 +57,102 @@ function SetupReleasesFlow() {
   return (
     <Fragment>
       <Head title='Releases setup' />
-      <GeneralContextProvider value={{ state, update }}>
-        <CommonDialog minHeight={height} {...disclosure} onClose={onClose}>
-          <CommonDialogHeader label='' />
-          <CommonDialogContent>
-            <VStack px={6} h='100%' justify='space-between'>
-              <Outlet />
-              <Box
-                position='sticky'
-                b={0}
-                pt={3}
-                pb={4}
-                zIndex={10}
-                css={{ backgroundColor: '#fff' }}
-              >
-                <StepperIndicator current={getStepNumber(currentPath)}>
-                  {arrayFrom(2).map((idx) => (
-                    <StepperIndicatorStep key={idx} />
-                  ))}
-                </StepperIndicator>
-              </Box>
-            </VStack>
-          </CommonDialogContent>
-          {stepNumber < 2 && (
-            <CommonDialogActionButton
-              label='Continue'
-              onClick={() => {
-                if (stepNumber === 1) {
-                  setHeight('650px');
-                }
-
-                const nextStep = (stepNumber + 1) as StepNumber;
-                navigate(
-                  makePath([
-                    Paths.setupFlow,
-                    Paths.releases,
-                    StepMap[nextStep],
-                  ]),
-                  {
-                    state: {
-                      previousLocation:
-                        location.state?.previousLocation || location,
-                    },
-                  },
-                );
-              }}
-            />
-          )}
-          {stepNumber === 2 && (
-            <CommonDialogActionButton
-              label={
-                state.ids.length > 0
-                  ? `Follow (${state.ids.length})`
-                  : 'Skip'
-              }
-              onClick={onClose}
-            />
-          )}
-        </CommonDialog>
-      </GeneralContextProvider>
+      <GQLRenderer ErrorFallback={() => <Fragment />}>
+        <SetupDialog />
+      </GQLRenderer>
     </Fragment>
   );
 }
 SetupReleasesFlow.displayName = 'Setup Releases Flow';
+
+function SetupDialog() {
+  const connectedAccounts = useConnectedAccounts();
+
+  const hasConnectedAccounts = connectedAccounts.edges.length > 0;
+
+  const disclosure = useDisclosure(true);
+
+  const [height, setHeight] = useState('550px');
+  const [state, update] = useRecordState<IUnconnectedDialogContext>({
+    ids: [],
+  });
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const paths = location.pathname.split('/');
+  const currentPath = paths[paths.length - 1];
+  const stepNumber = getStepNumber(currentPath);
+
+  const onClose = () => {
+    const previousLocation = location.state?.previousLocation;
+
+    update({ ids: [] });
+    setHeight('550px');
+    navigate(previousLocation || prefix('/', Paths.releases));
+  };
+
+  return (
+    <GeneralContextProvider value={{ state, update }}>
+      <CommonDialog minHeight={height} {...disclosure} onClose={onClose}>
+        <CommonDialogHeader label='' />
+        <CommonDialogContent>
+          <VStack px={6} h='100%' justify='space-between'>
+            <Outlet />
+            <Box
+              position='sticky'
+              b={0}
+              pt={3}
+              pb={4}
+              zIndex={10}
+              css={{ backgroundColor: '#fff' }}
+            >
+              <StepperIndicator current={getStepNumber(currentPath)}>
+                {arrayFrom(2).map((idx) => (
+                  <StepperIndicatorStep key={idx} />
+                ))}
+              </StepperIndicator>
+            </Box>
+          </VStack>
+        </CommonDialogContent>
+        {stepNumber < 2 && (
+          <CommonDialogActionButton
+            disabled={stepNumber === 1 && !hasConnectedAccounts}
+            label='Continue'
+            onClick={() => {
+              if (stepNumber === 1) {
+                setHeight('650px');
+              }
+
+              const nextStep = (stepNumber + 1) as StepNumber;
+              navigate(
+                makePath([
+                  Paths.setupFlow,
+                  Paths.releases,
+                  StepMap[nextStep],
+                ]),
+                {
+                  state: {
+                    previousLocation:
+                      location.state?.previousLocation || location,
+                  },
+                },
+              );
+            }}
+          />
+        )}
+        {stepNumber === 2 && (
+          <CommonDialogActionButton
+            label={
+              state.ids.length > 0
+                ? `Follow (${state.ids.length})`
+                : 'Skip'
+            }
+            onClick={onClose}
+          />
+        )}
+      </CommonDialog>
+    </GeneralContextProvider>
+  );
+}
 
 export default SetupReleasesFlow;
