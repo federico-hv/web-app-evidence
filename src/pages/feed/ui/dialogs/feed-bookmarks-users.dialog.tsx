38e0d96221e82e444be13727c53574a88fe15bdb
Babak <hadadybabak@gmail.com>
2023-12-04 12:03:30 -0800

    feat: [views] bookmark query integration

--- src/pages/feed/ui/dialogs/feed-bookmarks-users.dialog.tsx
+++ src/pages/feed/ui/dialogs/feed-bookmarks-users.dialog.tsx
@@ -1,10 +1,23 @@
+import { useSuspenseQuery } from '@apollo/client';
+import { GET_BOOKMARKED_USERS } from '../../queries';
 import {
   CommonDialog,
   CommonDialogContent,
   CommonDialogHeader,
   EmptyMessage,
+  GQLRenderer,
+  IConnection,
+  IPaginationParams,
+  UserModel,
+  useDialogContext,
   useDialogTabContext,
 } from 'shared';
+import {
+  UserWithRelationshipAction,
+  useFeedContext,
+} from '../../../../features';
+import { Box, VStack } from '@holdr-ui/react';
+import { Fragment } from 'react';
 
 function FeedBookmarksUsersDialog() {
   const { isOpen, onOpen, onClose, option } = useDialogTabContext();
@@ -28,11 +41,44 @@ function FeedBookmarksUsersDialog() {
 
 // TODO: integrate bookmarks query
 function BookmarkUsersList() {
+  const { feedId } = useFeedContext();
+  const { onClose } = useDialogContext();
+
+  function List() {
+    const { data } = useSuspenseQuery<
+      { usersWhoBookmarked: IConnection<UserModel, string> },
+      { id?: string; params?: IPaginationParams<string> }
+    >(GET_BOOKMARKED_USERS, {
+      variables: { id: feedId },
+      fetchPolicy: 'network-only',
+    });
+
+    const bookmarkedUsers = data?.usersWhoBookmarked?.edges;
+    return (
+      <Box borderTop={1} borderColor='base100' mt='calc(-1 * $3)' pt={4}>
+        {bookmarkedUsers && bookmarkedUsers.length > 0 ? (
+          <VStack gap={4}>
+            {bookmarkedUsers.map((item, idx) => (
+              <UserWithRelationshipAction
+                data={item.node}
+                key={`bookmarked-user-${idx}`}
+                onClose={onClose}
+              />
+            ))}
+          </VStack>
+        ) : (
+          <EmptyMessage
+            title='No bookmarks yet.'
+            subtitle='Nobody has bookmarked your post yet.'
+          />
+        )}
+      </Box>
+    );
+  }
   return (
-    <EmptyMessage
-      title='No bookmarks yet.'
-      subtitle='Nobody has bookmarked your post yet.'
-    />
+    <GQLRenderer ErrorFallback={() => <Fragment />}>
+      <List />
+    </GQLRenderer>
   );
 }
 
