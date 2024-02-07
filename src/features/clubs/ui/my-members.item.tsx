afd1e92bb5a10a14afc4611f9e6379538975e559
liangtam <tammieliang@hotmail.com>
2024-02-06 17:59:38 -0800

    refactor: members list item using dynamic values (not exact to figma design)

--- src/features/clubs/ui/my-members.item.tsx
+++ src/features/clubs/ui/my-members.item.tsx
@@ -15,27 +15,27 @@ import {
   
   function MyMembersItem({ data, online }: MyMembersItemProps) {
     return (
-      <HStack p={'8px'} gap={4} radius={2} items={'center'}>
-        <Avatar size='base' src={data.avatar}>
-          {online && (
-            <AvatarBadge size={1}>
-              <Circle
-                size='13px'
-                css={{ backgroundColor: '#34C05A', borderColor: '#141317' }}
-                border={1}
-              />
-            </AvatarBadge>
-          )}
-        </Avatar>
-        <VStack gap={2}>
-          <Text size='16px' weight={500}>
-            {data.displayName}
-          </Text>
-          <Text size='12px' weight={300}>
-            {`@${data.username}`}
-          </Text>
-        </VStack>
-      </HStack>
+      <HStack p={2} gap={4} radius={2} items={'center'}>
+      <Avatar size='base' src={data.avatar}>
+        {online && (
+          <AvatarBadge size={1}>
+            <Circle
+              size='13px'
+              css={{ backgroundColor: '#34C05A', borderColor: '#141317' }}
+              border={1}
+            />
+          </AvatarBadge>
+        )}
+      </Avatar>
+      <VStack gap={2}>
+        <Text size={3} weight={500}>
+          {data.displayName}
+        </Text>
+        <Text size={1} weight={300}>
+          {`@${data.username}`}
+        </Text>
+      </VStack>
+    </HStack>
     );
   }
   
