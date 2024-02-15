3cbef488877d61accbbaae3875dbb906275fca61
liangtam <tammieliang@hotmail.com>
2024-02-15 14:21:54 -0800

    refactor: prettier

--- src/features/clubs/ui/membership-item/membership-item.tsx
+++ src/features/clubs/ui/membership-item/membership-item.tsx
@@ -1,4 +1,11 @@
-import { Avatar, HStack, VStack, Text, Icon, theme } from '@holdr-ui/react';
+import {
+  Avatar,
+  HStack,
+  VStack,
+  Text,
+  Icon,
+  theme,
+} from '@holdr-ui/react';
 import { TextGroup, TextGroupHeading, TextGroupSubheading } from 'shared';
 import { MembershipItemProps } from './membership-item.types';
 
@@ -18,14 +25,32 @@ function MembershipItem({ data }: MembershipItemProps) {
       <Avatar size='xl' src={data.artist.avatar} variant='squircle' />
       <VStack gap={2}>
         <TextGroup gap={0}>
-          <TextGroupHeading size={2}css={{lineHeight: 1.45}}>{data.name}</TextGroupHeading>
-          <TextGroupSubheading size={1} weight={300} color='white700' css={{lineHeight: 1}}>
+          <TextGroupHeading size={2} css={{ lineHeight: 1.45 }}>
+            {data.name}
+          </TextGroupHeading>
+          <TextGroupSubheading
+            size={1}
+            weight={300}
+            color='white700'
+            css={{ lineHeight: 1 }}
+          >
             {`Membership #${data.membershipNum}`}
           </TextGroupSubheading>
         </TextGroup>
         <HStack gap={1} items='center'>
-          <Icon name={data.priceHasRisen ? 'arrow-up-outline' : 'arrow-down-outline'} color={data.priceHasRisen ? 'success500' : 'danger400'} />
-          <Text weight={500} size={2} color={data.priceHasRisen ? 'success500' : 'danger400'}>
+          <Icon
+            name={
+              data.priceHasRisen
+                ? 'arrow-up-outline'
+                : 'arrow-down-outline'
+            }
+            color={data.priceHasRisen ? 'success500' : 'danger400'}
+          />
+          <Text
+            weight={500}
+            size={2}
+            color={data.priceHasRisen ? 'success500' : 'danger400'}
+          >
             {`$${data.priceChange.toFixed(2)} USD`}
           </Text>
         </HStack>
