da8d03a9cc0eb06ba98cca33b66a338d1a0e376c
liangtam <tammieliang@hotmail.com>
2024-02-22 11:25:16 -0800

    refactor: follow better practices

--- src/features/clubs/ui/groups/membership-item/__tests__/membership-item.test.tsx
+++ src/features/clubs/ui/groups/membership-item/__tests__/membership-item.test.tsx
@@ -1,54 +1,90 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { describe, it, expect } from 'vitest';
+import { vi, describe, it, expect } from 'vitest';
 import MembershipItem from '../membership-item';
-import { dummyOwnedMembershipData, dummyOwnedMembershipData2 } from './dummyData';
+import {
+  dummyOwnedMembershipData,
+  dummyOwnedMembershipData2,
+} from './dummyData';
 
-describe("MembershipItem tests", () => {
-    it("should render membership item with rising price", () => {
-        render(<MembershipItem data={dummyOwnedMembershipData}/>);
+type ImageConstructor = new (
+  width?: number | undefined,
+  height?: number | undefined,
+) => HTMLImageElement;
 
-        const avatarElement = screen.getByRole('complementary');
-        expect(avatarElement).toBeInTheDocument();
+describe('MembershipItem tests', () => {
+  global.Image = class {
+    onload: () => void;
 
-        const nameElement = screen.getByTestId('membership-item-name');
-        expect(nameElement).toBeInTheDocument();
+    constructor() {
+      this.onload = vi.fn();
+      setTimeout(() => {
+        this.onload();
+      }, 50);
+    }
+  } as unknown as ImageConstructor;
 
-        const numberElement = screen.getByTestId('membership-item-number');
-        expect(numberElement).toBeInTheDocument();
-        expect(numberElement).toHaveTextContent(`#${dummyOwnedMembershipData.membershipNum}`);
+  it('should render membership item with rising price', async () => {
+    render(<MembershipItem data={dummyOwnedMembershipData} />);
 
-        // screen.debug();
-        
-        const iconElement = screen.getByTestId('membership-item-change-icon');
-        expect(iconElement).toBeInTheDocument();
-        // expect(iconElement).toHaveProperty("name", "arrow-up-outline");
-        
-        const priceChangeElement = screen.getByTestId('membership-item-priceChange');
-        expect(priceChangeElement).toBeInTheDocument();
-        expect(priceChangeElement).toHaveTextContent(`$${dummyOwnedMembershipData.priceChange.toFixed(2)} USD`);
-
+    await waitFor(() => {
+      const avatarElement = screen.getByRole('img');
+      expect(avatarElement).toBeInTheDocument();
     });
 
-    it("should render membership item with falling price", () => {
-        render(<MembershipItem data={dummyOwnedMembershipData2}/>);
+    const nameElement = screen.getByLabelText('membership-item name');
+    expect(nameElement).toBeInTheDocument();
+    expect(nameElement).toHaveTextContent(dummyOwnedMembershipData.name);
 
-        const avatarElement = screen.getByRole('complementary');
-        expect(avatarElement).toBeInTheDocument();
+    const numberElement = screen.getByLabelText('membership-item number');
+    expect(numberElement).toBeInTheDocument();
+    expect(numberElement).toHaveTextContent(
+      `Membership #${dummyOwnedMembershipData.membershipNum}`,
+    );
 
-        const nameElement = screen.getByTestId('membership-item-name');
-        expect(nameElement).toBeInTheDocument();
+    const increaseIconElement = screen.getByLabelText('increase in value');
+    expect(increaseIconElement).toBeInTheDocument();
+    const decreaseIconElement = screen.queryByLabelText('decrease in value');
+    expect(decreaseIconElement).not.toBeInTheDocument();
 
-        const numberElement = screen.getByTestId('membership-item-number');
-        expect(numberElement).toBeInTheDocument();
-        expect(numberElement).toHaveTextContent(`#${dummyOwnedMembershipData2.membershipNum}`);
+    const priceChangeElement = screen.getByLabelText(
+      'membership-item price change',
+    );
+    expect(priceChangeElement).toBeInTheDocument();
+    expect(priceChangeElement).toHaveTextContent(
+      `$${dummyOwnedMembershipData.priceChange.toFixed(2)} USD`,
+    );
+  });
 
-        const iconElement = screen.getByTestId('membership-item-change-icon');
-        expect(iconElement).toBeInTheDocument();
-        
-        const priceChangeElement = screen.getByTestId('membership-item-priceChange');
-        expect(priceChangeElement).toBeInTheDocument();
-        expect(priceChangeElement).toHaveTextContent(`$${dummyOwnedMembershipData2.priceChange.toFixed(2)} USD`);
+  it('should render membership item with falling price', async () => {
+    render(<MembershipItem data={dummyOwnedMembershipData2} />);
 
+    await waitFor(() => {
+      const avatarElement = screen.getByRole('img');
+      expect(avatarElement).toBeInTheDocument();
     });
-})
+
+    const nameElement = screen.getByLabelText('membership-item name');
+    expect(nameElement).toBeInTheDocument();
+    expect(nameElement).toHaveTextContent(dummyOwnedMembershipData2.name);
+
+    const numberElement = screen.getByLabelText('membership-item number');
+    expect(numberElement).toBeInTheDocument();
+    expect(numberElement).toHaveTextContent(
+      `Membership #${dummyOwnedMembershipData2.membershipNum}`,
+    );
+
+    const decreaseIconElement = screen.getByLabelText('decrease in value');
+    expect(decreaseIconElement).toBeInTheDocument();
+    const increaseIconElement = screen.queryByLabelText('increase in value');
+    expect(increaseIconElement).not.toBeInTheDocument();
+
+    const priceChangeElement = screen.getByLabelText(
+      'membership-item price change',
+    );
+    expect(priceChangeElement).toBeInTheDocument();
+    expect(priceChangeElement).toHaveTextContent(
+      `$${dummyOwnedMembershipData.priceChange.toFixed(2)} USD`,
+    );
+  });
+});
