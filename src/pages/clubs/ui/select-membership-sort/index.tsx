969462c4977f901cfebc540458c32e5ceefbcc87
liangtam <tammieliang@hotmail.com>
2024-04-08 15:13:53 -0700

    refactor: update colours

--- src/pages/clubs/ui/select-membership-sort/index.tsx
+++ src/pages/clubs/ui/select-membership-sort/index.tsx
@@ -30,12 +30,12 @@ function SelectMembershipsSort() {
           placeholder='Sort By'
           css={{
             border: '1px solid rgba(152, 152, 255, 0.10)',
-            background: 'rgba(133, 133, 255, 0.2)',
+            background: 'rgba(152, 152, 255, 0.1)',
           }}
         />
         <SelectContent sticky='always'>
           <SelectItemList
-            _active={{ color: '$purple400' }}
+            _active={{ color: '$purple200' }}
             w={190}
             divider={
               <Box
@@ -49,7 +49,7 @@ function SelectMembershipsSort() {
             position='relative'
             css={{
               boxShadow: '0px 0px 100px 0px rgba(14, 14, 27, 0.35)',
-              backgroundColor: 'rgba(49, 49, 73, 0.75)',
+              backgroundColor: 'rgba(49, 49, 73, 0.85)',
               backdropFilter: 'blur(50px)',
               borderBottomLeftRadius: '$2',
               borderBottomRightRadius: '$2',
