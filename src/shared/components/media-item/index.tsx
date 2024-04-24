b2bc6611ea09d1adb61c07d48540f890e8e6aa84
Takudzwa Mhonde <tmhonde45@gmail.com>
2024-04-24 11:18:17 -0700

    fix: add fallback src to image
    Signed-off-by: Takudzwa Mhonde <tmhonde45@gmail.com>

--- src/shared/components/media-item/index.tsx
+++ src/shared/components/media-item/index.tsx
@@ -12,6 +12,7 @@ import Video, {
   VideoThumbnail,
   VideoVolume,
 } from '../video';
+import { Asset } from '../../constants';
 
 function MediaItem({
   url,
@@ -54,14 +55,15 @@ function MediaItem({
               fit='cover'
               src={url}
               alt={title}
+              fallbackSrc={Asset.Image.DarkPlaceholder}
               css={{ filter: 'blur(24px)', scale: 2 }}
             />
           )}
           <Box w='full' h='full' position='absolute' t={0} r={0}>
             <Image
-              className={CenteredImage()}
               src={url}
               alt={title}
+              fallbackSrc={Asset.Image.DarkPlaceholder}
               fit='contain'
             />
           </Box>
