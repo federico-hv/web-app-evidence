978cdaf97bf10128acd425944a277b530ff24678
Takudzwa Mhonde <tmhonde45@gmail.com>
2024-04-29 17:27:14 -0700

    feat: create setup artist profile views
    Signed-off-by: Takudzwa Mhonde <tmhonde45@gmail.com>

---
+++ src/features/artist/ui/index.ts
@@ -0,0 +1,5 @@
+export { default as AboutMeAndPerksView } from './about-me-and-perks.view';
+export { default as SocialMediaAccountsView } from './social-media-accounts.view';
+export { default as UploadPhotoView } from './upload-photo.view';
+export { default as CustomURLView } from './custom-url.view';
+export { default as ConnectOnboardingView } from './connect-onboarding.view';
