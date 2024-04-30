5b45c6665389e9c41677a80bf51a46671ad3c9ed
Takudzwa Mhonde <tmhonde45@gmail.com>
2024-04-29 17:29:13 -0700

    feat: add setup page step
    Signed-off-by: Takudzwa Mhonde <tmhonde45@gmail.com>

---
+++ src/pages/setup-flow/artist-profile/ui/setup-step/index.tsx
@@ -0,0 +1,35 @@
+import { Box, Circle, HStack, Text } from '@holdr-ui/react';
+import { LinkOverlay, makePath, Paths } from '../../../../../shared';
+import { SetupStepProps } from './types';
+
+function SetupStep({ number, path, description, active }: SetupStepProps) {
+  return (
+    <Box position='relative'>
+      <LinkOverlay
+        to={makePath([Paths.setupProfile, Paths.artist, path])}
+      />
+      <HStack items='center' gap={3}>
+        <Circle
+          fontSize={2}
+          size='20px'
+          color={active ? '#30304B' : 'white700'}
+          bgColor={active ? 'purple200' : undefined}
+          border={active ? 0 : 1}
+          borderColor={active ? undefined : 'white700'}
+        >
+          {number}
+        </Circle>
+        <Text
+          weight={active ? 500 : 400}
+          casing='capitalize'
+          color={active ? 'purple200' : 'white700'}
+        >
+          {description}
+        </Text>
+      </HStack>
+    </Box>
+  );
+}
+SetupStep.displayName = 'ArtistSetupStep';
+
+export default SetupStep;
