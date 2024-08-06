368569e329ed50c713fe23ea3f7335fbd09f4037
Takudzwa Mhonde <tmhonde45@gmail.com>
2024-08-06 08:25:59 -0700

    build: add logrocket

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
 import './styles/index.css';
+import LogRocket from 'logrocket';
+
+LogRocket.init('weponz/holdr-staging');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
