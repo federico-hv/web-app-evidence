aedbac6281adacab6199487f2f33bb28857cf1b1
Takudzwa Mhonde <tmhonde45@gmail.com>
2024-08-28 18:35:14 -0700

    fix: remove poll dialog

--- src/features/feeds/ui/groups/polls/index.tsx
+++ src/features/feeds/ui/groups/polls/index.tsx
@@ -1,23 +1,17 @@
 import { Fragment } from 'react';
 import { useFeedContext, useVotePoll } from '../../../shared';
-import { VStack, useDisclosure } from '@holdr-ui/react';
+import { VStack } from '@holdr-ui/react';
 import dayjs from 'dayjs';
-import {
-  CustomSkeleton,
-  DialogContextProvider,
-  Loader,
-} from '../../../../../shared';
+import { CustomSkeleton, Loader } from '../../../../../shared';
 import { PollResponse } from '../index';
 import { AnswerPollButton } from '../../buttons';
 import { PollsProps } from './types';
-import { PollVotesDialog } from '../../dialogs';
 import { FlatList } from '../../../../../tmp/flat-list';
 import { useCurrentUser } from '../../../../auth';
 
 function Polls({ id, items, endDate }: PollsProps) {
   const currentUser = useCurrentUser();
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const { feedId, owner } = useFeedContext();
 
   const { votePoll, loading } = useVotePoll();
@@ -29,46 +23,43 @@ function Polls({ id, items, endDate }: PollsProps) {
   const expired = dayjs(dayjs()).isAfter(endDate);
 
   return (
-    <DialogContextProvider value={{ isOpen, onOpen, onClose }}>
-      <Loader
-        loading={loading}
-        h='fit-content'
-        as={
-          <FlatList
-            w='100%'
-            direction='vertical'
-            gap={3}
-            mt={4}
-            pl='6px'
-            data={items}
-            renderItem={(item) => (
-              <CustomSkeleton radius={1} h={6} w='100%' />
-            )}
-            keyExtractor={(item) => item.id}
-          />
-        }
-      >
-        <VStack position='relative' zIndex={5} gap={5}>
-          <VStack gap={3} mt={5} pl='6px'>
-            {items.map((data) => (
-              <Fragment key={`poll-${data.id}`}>
-                {!voted && !expired && owner.id !== currentUser.id ? (
-                  <AnswerPollButton
-                    onClick={async () =>
-                      votePoll(feedId, id, data.id as number)
-                    }
-                    label={data.text}
-                  />
-                ) : (
-                  <PollResponse total={total} data={data} />
-                )}
-              </Fragment>
-            ))}
-          </VStack>
+    <Loader
+      loading={loading}
+      h='fit-content'
+      as={
+        <FlatList
+          w='100%'
+          direction='vertical'
+          gap={3}
+          mt={4}
+          pl='6px'
+          data={items}
+          renderItem={(item) => (
+            <CustomSkeleton radius={1} h={6} w='100%' />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      }
+    >
+      <VStack position='relative' zIndex={5} gap={5}>
+        <VStack gap={3} mt={5} pl='6px'>
+          {items.map((data) => (
+            <Fragment key={`poll-${data.id}`}>
+              {!voted && !expired && owner.id !== currentUser.id ? (
+                <AnswerPollButton
+                  onClick={async () =>
+                    votePoll(feedId, id, data.id as number)
+                  }
+                  label={data.text}
+                />
+              ) : (
+                <PollResponse total={total} data={data} />
+              )}
+            </Fragment>
+          ))}
         </VStack>
-      </Loader>
-      <PollVotesDialog items={items} />
-    </DialogContextProvider>
+      </VStack>
+    </Loader>
   );
 }
 Polls.displayName = 'Polls';
